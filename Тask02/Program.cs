8ff45a2d338137315ec0f42bd43a3da341d6c724
Mariya <dr.zhestkova@gmail.com>
2023-10-13 20:42:48 +0300

    Генерация случайного числа и поиск наибольшей цифры числа

---
+++ Тask02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. 78->8 12-> 2 85->8
+int number = new Random().Next(10, 100); // сгенерировали случайное число в диапозоне от 10 до 99 и переместили в переменную number
+Console.WriteLine(number);
+int FirstDigit = number / 10;
+int LastDigit = number % 10; 
+if(FirstDigit > LastDigit)
+{
+    Console.WriteLine(FirstDigit);
+}
+else 
+{
+    Console.WriteLine(LastDigit);
+}
