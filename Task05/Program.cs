0e6f3500f8b76d11a6907379ba64a1065dc5186e
Mariya <dr.zhestkova@gmail.com>
2023-10-13 21:36:59 +0300

    Поиск второй цифры в трехзначном числе

---
+++ Task05/Program.cs
@@ -0,0 +1,21 @@
+﻿// SЗадача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int number1 = ReadInt("Введите трехзначное число");
+int result;
+if (number1 > 99 && number1 < 1000)
+{
+    result = number1 / 10 % 10;
+    Console.WriteLine(result);
+}
+else Console.WriteLine("введенное число не трехзначное");
