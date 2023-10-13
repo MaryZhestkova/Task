658cba8a28cf233be3b3245b3a3e257bd0280961
Mariya <dr.zhestkova@gmail.com>
2023-10-13 19:21:19 +0300

    Поиск трехзначного числа

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int number=ReadInt("Введите трехзначное число ");
+if (number > 99 & number <1000)
+{
+    int LastDigit = number % 10;
+    Console.WriteLine("Третье число = " + LastDigit);
+}
+else Console.WriteLine("Число не трехзначное");
