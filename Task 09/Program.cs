ee465c34f91df766af562d5f52980701c4cb21e6
Mariya <dr.zhestkova@gmail.com>
2023-10-14 17:08:47 +0300

    Возведение числа в степень

---
+++ Task 09/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведение числа в степень
+
+int power(int a, int b){
+    int result = 1;
+    for (int i = 1; i<=b; i++)
+    result = result * a;
+    return result;
+}
+Console.WriteLine("Ввведите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввведите cтепень: ");
+int stepen = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(power(chislo, stepen));
---
+++ Task08/Program.cs
@@ -0,0 +1,41 @@
+﻿// Написать игру - компьютер загадывает рандомное число, от 1 до 99 включительно. 
+// Пользователь вводит число наугад, компьютер в зависимости от результата пишет больше или меньше 
+// введенное пользователем число, чем число загаданное компьютером. Когда пользователь угадывает число, 
+// компьютер поздравляет его с победой, выводит число на экран, и программа завершается
+
+// // Подсказка: рекомендую while
+
+int number = new Random().Next(1, 100); // число от 10 до 99 и переместили в переменную number
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int anyNumber = ReadInt("Введите число от 1 до 99");
+if (anyNumber == number)
+{
+    Console.WriteLine("Ура! С победой! Загаданное число " + number);
+    break;
+}
+else if (anyNumber < 1 || anyNumber > 99)
+{
+    Console.WriteLine("Введенное число не соответствует критериям ввода");
+    break;
+}
+else
+{
+    while (anyNumber > number)
+    {
+        Console.WriteLine("больше");
+        int anyNumber = ReadInt("Ввведите число от 1 до 99");
+    }
+    while (anyNumber < number)
+    {
+        Console.WriteLine("меньше");
+        int anyNumber = ReadInt("Ввведите число от 1 до 99");
+    }
+}
