02ad9b64cd6fe1f38288f502c2b449d1efb47bab
Mariya <dr.zhestkova@gmail.com>
2023-10-23 21:40:04 +0300

    Домашка № 4. Задача 2

---
+++ Task12/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int result;
+    result = int.Parse(numb);
+    return result;
+} 
+
+int SumOfDigits (int number)
+{
+    int result = 0; 
+    while (number > 0)
+    {
+        result += number %10;
+        number = number / 10;
+    }
+    return result; 
+}
+int number = ReadInt("Введите число");
+System.Console.WriteLine($"Сумма всех цифр числа {number} равна {SumOfDigits(number)}");
